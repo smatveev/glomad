9b5a77adbd509b8dea9aeecbfdc8889ab0f8b542
NITRO\smatveev <matv33v@gmail.com>
2021-05-17 15:28:41 +0300

    Update agular ver

--- Glomad.net/Startup.cs
+++ Glomad.net/Startup.cs
@@ -45,7 +45,7 @@ namespace Glomad.net
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "AngularApp/dist";
             });
         }
 
@@ -89,7 +89,7 @@ namespace Glomad.net
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "AngularApp";
 
                 if (env.IsDevelopment())
                 {
