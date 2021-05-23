4e6c4177f99d24b76f2b017a83a1ee193dc5c152
NITRO\smatveev <matv33v@gmail.com>
2021-05-23 15:03:54 +0300

    New template with asp core backend

---
+++ Glomad.net/Glomad.net/Shared/Routes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glomad.net.Shared
+{
+    class Routes
+    {
+        public List<Country> countries;
+
+        public Routes()
+        {
+            countries = new List<Country>
+            {
+                new Country {Name = "UK"},
+                new Country {Name = "US"},
+                new Country {Name = "Russia"}
+            };
+        }
+    }
+
+    class Country
+    {
+        public string Name { get; set; }
+    }
+}
