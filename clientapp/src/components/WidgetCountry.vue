a013352716030dca2f5359f3242b260433b6cc27
NITRO\smatveev <matv33v@gmail.com>
2021-09-02 16:21:21 +0300

    add country widget for country page

---
+++ clientapp/src/components/WidgetCountry.vue
@@ -0,0 +1,20 @@
+<template>
+    <div ref="d"></div>
+</template>
+
+
+<script>
+
+export default ({
+    props: ['to'],
+    mounted() {
+      let script = document.createElement('script')
+      script.setAttribute('src', "https://tp.media/content?currency=usd&promo_id=4044&shmarker=14510.glomad&campaign_id=100&trs=21652&target_host=search.jetradar.com&locale=en_us&limit=6&powered_by=false&destination=" + this.to)
+      script.setAttribute('charset', "utf-8")
+      this.$refs['d'].appendChild(script)
+
+      console.log("widget country capital = " + this.to);
+    },
+ 
+})
+</script>
--- clientapp/src/views/Country.vue
+++ clientapp/src/views/Country.vue
@@ -36,31 +36,34 @@
       <div class="tab-pane fade" :class="{ 'active show': isActive('Visas') }" id="Visas">
             <h2 class="fw-bold text-center p-4">💳 List of {{country.name}} visa types</h2>
 
-<div class="row row-cols-sm-1  row-cols-lg-3 row-cols-md-2 g-4">
-<div class="py-2" v-for="v in visas" :key="v.id">
-      <div class="p-3 bg-light rounded-3">
-        <div class="container-fluid">
-          <h5 class="fw-bold">{{ v.visaName }}</h5>
+      <div class="row row-cols-sm-1  row-cols-lg-3 row-cols-md-2 g-4">
+        <div v-for="v in visas" :key="v.id">
+              <div class="p-3 m-1 bg-light rounded-3">
+                <div class="container-fluid">
+                  <h5 class="fw-bold">{{ v.visaName }}</h5>
 
-          <p class="col-md-12">{{ v.description }}</p>
+                  <p class="col-md-12">{{ v.description }}</p>
 
-          <span
-            v-if="v.isExdendable"
-            class="badge badge-card bg-info text-dark m-2 fs-6"
-          >
-            ✨ Extendable
-          </span>
+                  <span
+                    v-if="v.isExdendable"
+                    class="badge badge-card bg-info text-dark m-2 fs-6"
+                  >
+                    ✨ Extendable
+                  </span>
 
-          <span class="badge badge-card bg-info text-dark m-2 fs-6">
-            🕜 Duration {{ v.duration }} days
-          </span>
+                  <span class="badge badge-card bg-info text-dark m-2 fs-6">
+                    🕜 Duration {{ v.duration }} days
+                  </span>
+                </div>
+            </div>
+            
         </div>
-      </div>
-    </div>
 
-    <div class="p-3 bg-light rounded-3">
-        <W/>
-      </div>
+        <div class="p-2 bg-light rounded-3">
+          <WC v-if="country.capitalCode" :to="country.capitalCode" />
+        </div>
+
+
     </div>
       </div>
 
@@ -85,7 +88,9 @@
           <h2 class="fw-bold text-center p-4">🦠 {{country.name}} COVID-19 travel requirements</h2>
 
           <p class="p-3" v-html="covidInfo"></p>
+
         </section>
+
         </div>
     </div>
 
@@ -110,26 +115,25 @@
 </template>
 
 <script>
-
 // import Tabs from '@/components/Tabs.vue'
 // import Tab from '@/components/Tab.vue'
-import moment from 'moment'
+import moment from "moment";
 
-import W from '@/components/Widget.vue'
+import WC from "@/components/WidgetCountry.vue";
 
 export default {
   //props: ['id'],
   data() {
     return {
-      activeItem: '',
+      activeItem: "",
       country: {},
       name: this.$route.params.name,
       hash: this.$route.hash,
       embassy: {},
-      
+
       visasNonEntry: {},
       visas: {},
-      covidInfo: ""
+      covidInfo: "",
     };
   },
   created() {
@@ -137,7 +141,8 @@ export default {
     this.setActive(this.hash.substring(1) ? this.hash.substring(1) : "Visas");
   },
   mounted() {
-    document.title = this.$route.params.name + " travel COVID-19 requirements - Glomad"
+    document.title =
+      this.$route.params.name + " travel COVID-19 requirements - Glomad";
 
     fetch(process.env.VUE_APP_API_URL + "Countries/GetByName?name=" + this.name)
       .then((res) => res.json())
@@ -147,7 +152,11 @@ export default {
       })
       .catch((err) => console.log(err.message));
 
-    fetch(process.env.VUE_APP_API_URL + "Countries/GetCountryEmbassies?country=" + this.name)
+    fetch(
+      process.env.VUE_APP_API_URL +
+        "Countries/GetCountryEmbassies?country=" +
+        this.name
+    )
       .then((res) => res.json())
       .then((data) => {
         this.embassy = data;
@@ -158,44 +167,44 @@ export default {
         this.embassy = null;
       });
 
-
     fetch(process.env.VUE_APP_API_URL + "Visas?country=" + this.name)
-        .then((res) => res.json())
-        .then((data) => {
-          this.visas = data;
-          console.info(data);
-        })
-        .catch((err) => {
-          console.warn(err.message);
-          this.visas = [];
-        });
-    
-    fetch(process.env.VUE_APP_API_URL + "Countries/GetCovidInfo?name=" + this.name)
-        .then((res) => res.text())
-        .then((text) => (this.covidInfo = text))
-        .catch((err) => {
-          console.warn(err.message);
-        });
+      .then((res) => res.json())
+      .then((data) => {
+        this.visas = data;
+        console.info(data);
+      })
+      .catch((err) => {
+        console.warn(err.message);
+        this.visas = [];
+      });
+
+    fetch(
+      process.env.VUE_APP_API_URL + "Countries/GetCovidInfo?name=" + this.name
+    )
+      .then((res) => res.text())
+      .then((text) => (this.covidInfo = text))
+      .catch((err) => {
+        console.warn(err.message);
+      });
   },
   components: {
-   // Tabs,
-   // Tab,
-    W
+    // Tabs,
+    // Tab,
+    WC,
   },
   methods: {
     dateTime(value) {
-      return moment(value, "YYYYMMDDHHmmss").fromNow(); 
-      },
+      return moment(value, "YYYYMMDDHHmmss").fromNow();
+    },
     foo(p) {
-      alert(p)
+      alert(p);
     },
-    isActive (menuItem) {
-      return this.activeItem === menuItem
+    isActive(menuItem) {
+      return this.activeItem === menuItem;
     },
-    setActive (menuItem) {
-      this.activeItem = menuItem
-      
-    }
-  }
+    setActive(menuItem) {
+      this.activeItem = menuItem;
+    },
+  },
 };
 </script>
