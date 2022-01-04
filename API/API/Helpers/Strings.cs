a9d0fc81470db20cc5c4caabb904c6dc6d7921bc
NITRO\smatveev <matv33v@gmail.com>
2022-01-04 20:46:35 +0300

    G

--- API/API/Helpers/Strings.cs
+++ API/API/Helpers/Strings.cs
@@ -14,5 +14,13 @@ namespace API.Helpers
                 "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
                 _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
             };
+
+        public static string AddRelAttrs(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            return input.Replace("<a href", "<a rel=\"noopener nofollow noreferrer\" href");
+        }
     }
 }
