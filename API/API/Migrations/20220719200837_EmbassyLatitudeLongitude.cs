3982efa40a589b30ce9bbbc989fe224e5634811f
smatveev <matv33v@gmail.com>
2022-07-19 22:10:16 +0200

    long and lat

---
+++ API/API/Migrations/20220719200837_EmbassyLatitudeLongitude.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Glomad.Migrations
+{
+    public partial class EmbassyLatitudeLongitude : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Embassy",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Embassy",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Embassy");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Embassy");
+        }
+    }
+}
--- API/API/Models/Embassy.cs
+++ API/API/Models/Embassy.cs
@@ -34,5 +34,7 @@ namespace Glomad.Models
         [Required]
         public Country OriginalCountry { get; set; }
         public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
