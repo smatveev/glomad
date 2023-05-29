af11e63993af6f6d95b4fd4404e5eee1fb6426fa
smatveev <matv33v@gmail.com>
2023-05-29 14:21:26 +0700

    x

--- API/API/Controllers/CountryController.cs
+++ API/API/Controllers/CountryController.cs
@@ -173,7 +173,7 @@ namespace API.Controllers
                 amadeus = _context.AmadeusApi.SingleOrDefault();
             }
 
-            if (amadeus.CallsLimit < 200 && (curCountry.UpdateDate == null || curCountry.UpdateDate < DateTime.Now.AddDays(-15)))
+            if (amadeus.CallsLimit < 180 && (curCountry.UpdateDate == null || curCountry.UpdateDate < DateTime.Now.AddDays(-15)))
             {
                 if (!string.IsNullOrEmpty(curCountry.ISOalpha2))
                 {
