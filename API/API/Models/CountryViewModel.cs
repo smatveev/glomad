9e9652190d1b1ba3ab3fcf3eaf27706b6c0a22f7
smatveev <matv33v@gmail.com>
2023-06-30 11:08:25 +0700

    maps

--- API/API/Models/CountryViewModel.cs
+++ API/API/Models/CountryViewModel.cs
@@ -22,6 +22,8 @@ namespace API.Models
         public string Country { get; set;}
         public string City { get; set; }
         public string Iata { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 
     public struct UpdatedCountries
