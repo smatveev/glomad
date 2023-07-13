7cf7e11b83c6a16e5600dd9ddccc17b416ce250c
smatveev <matv33v@gmail.com>
2023-07-13 20:20:01 +0700

    passport page first

---
+++ API/API/Controllers/PassportController.cs
@@ -0,0 +1,43 @@
+﻿using API.Helpers;
+using API.Models;
+using Glomad.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace API.Controllers
+{
+    public class PassportController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PassportController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Passport/{country}")]
+        public IActionResult Index(string country)
+        {
+            string countryName = country.FirstCharToUpper();
+
+            PassportViewModel model = new PassportViewModel();
+            model.Country = _context.Country.FirstOrDefault(m => m.Name == country);
+
+            model.NoVisaCountries = _context.NoVisaEntry
+                .Where(i => i.CountryPassport.Id == model.Country.Id).ToList();
+
+            HeaderViewModel header = new HeaderViewModel();
+            header.Text = $"Detail information for cizitens of {countryName}";
+            header.CountryName = countryName;
+            model.Header = header;
+
+            return View(model);
+        }
+    }
+}
---
+++ API/API/Models/PassportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Glomad.Models;
+using System.Collections.Generic;
+
+namespace API.Models
+{
+    public class PassportViewModel
+    {
+        public Country Country { get; set; }
+        public List<NoVisaEntry> NoVisaCountries { get; set; }
+        public HeaderViewModel Header { get; set; }
+    }
+}
