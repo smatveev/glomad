6c67d70608f093f1495920a2b3c5609fd2038739
smatveev <matv33v@gmail.com>
2023-08-19 19:31:29 +0700

    x

--- API/API/Models/Visa.cs
+++ API/API/Models/Visa.cs
@@ -60,6 +60,12 @@ namespace API.Models
         public bool IsActual { get; set; }
         public bool IsAnnounced { get; set; }
         public bool IsFreeVisa { get; set; }
+
+        public byte TaxSize { get; set; }
+        public string TaxDescription { get; set; }
+
+        public byte ProcessingTimeUp { get; set; }
+        public byte ProcessingTimeTo { get; set; }
     }
 
     public enum VisaType
