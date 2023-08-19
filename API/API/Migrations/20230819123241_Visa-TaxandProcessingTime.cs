c161082c52370579fd0bd850dab54254c98f43b0
smatveev <matv33v@gmail.com>
2023-08-19 19:33:53 +0700

    x

---
+++ API/API/Migrations/20230819123241_Visa-TaxandProcessingTime.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Glomad.Migrations
+{
+    public partial class VisaTaxandProcessingTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ProcessingTimeTo",
+                table: "Visa",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "ProcessingTimeUp",
+                table: "Visa",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxDescription",
+                table: "Visa",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "TaxSize",
+                table: "Visa",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingTimeTo",
+                table: "Visa");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessingTimeUp",
+                table: "Visa");
+
+            migrationBuilder.DropColumn(
+                name: "TaxDescription",
+                table: "Visa");
+
+            migrationBuilder.DropColumn(
+                name: "TaxSize",
+                table: "Visa");
+        }
+    }
+}
